d55758fdc1c5da42f9f1a3e36c8b18d08abbc7d9
Matthew Mulford <mhmulford0@gmail.com>
2022-07-20 13:52:54 -0500

    use sqlite for local dev, pg for deployment

--- config/database.js
+++ config/database.js
@@ -1,11 +1,50 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+// if ((process.env.NODE_ENV = "development" ? true : "lol")) {
+//   return {
+//     connection: {
+//       client: "sqlite",
+//       connection: {
+//         filename: path.join(
+//           __dirname,
+//           "..",
+//           env("DATABASE_FILENAME", ".tmp/data.db")
+//         ),
+//       },
+//       useNullAsDefault: true,
+//     },
+//   };
+// }
+// module.exports = ({ env }) => ();
+
+module.exports = ({ env }) =>
+  process.env.NODE_ENV === "development"
+    ? {
+        connection: {
+          client: "sqlite",
+          connection: {
+            filename: path.join(
+              __dirname,
+              "..",
+              env("DATABASE_FILENAME", ".tmp/data.db")
+            ),
+          },
+          useNullAsDefault: true,
+        },
+      }
+    : {
+        connection: {
+          client: "postgres",
+          connection: {
+            host: env("DATABASE_HOST"),
+            port: env.int("DATABASE_PORT"),
+            database: env("DATABASE_NAME"),
+            user: env("DATABASE_USERNAME"),
+            password: env("DATABASE_PASSWORD"),
+            ssl: {
+              rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+            },
+          },
+          debug: false,
+        },
+      };
